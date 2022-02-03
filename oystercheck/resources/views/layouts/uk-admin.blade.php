f020e04d494f3661acb56c739acb484531771c1b
Michael Ozoudeh <jesmikky@gmail.com>
2022-02-03 14:27:32 +0100

    updates

--- oystercheck/resources/views/layouts/uk-admin.blade.php
+++ oystercheck/resources/views/layouts/uk-admin.blade.php
@@ -11,8 +11,9 @@
         <link rel="shortcut icon" href="{{asset('/assets/images/favicon.png')}}">
           <link href="{{asset('/plugins/sweet-alert2/sweetalert2.min.css')}}" rel="stylesheet" type="text/css">
         <link href="{{asset('/plugins/animate/animate.css')}}" rel="stylesheet" type="text/css">
+
         <!-- jvectormap -->
-        <link href="{{asset('/plugins/jvectormap/jquery-jvectormap-2.0.2.css')}}" rel="stylesheet">
+        <link href="../plugins/jvectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet">
         <!-- App css -->
          <link href="{{asset('/plugins/dropify/css/dropify.min.css')}}" rel="stylesheet" type="text/css" />
         <link href="{{asset('/assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
@@ -20,13 +21,15 @@
         <link href="{{asset('/assets/css/metisMenu.min.css')}}" rel="stylesheet" type="text/css" />
         <link href="{{asset('/plugins/daterangepicker/daterangepicker.css')}}" rel="stylesheet" type="text/css" />
         <link href="{{asset('/assets/css/app.min.css')}}" rel="stylesheet" type="text/css" />
-       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA==" crossorigin="anonymous" referrerpolicy="no-referrer" /><meta name="csrf-token" content="{{ csrf_token() }}">
+   
+        <meta name="csrf-token" content="{{ csrf_token() }}">
     </head>
   <body class="dark-sidenav navy-sidenav">
-@include('partials.sidebar')
+@include('admin-layout.sidebar')
  <div class="page-wrapper">
-@include('partials.navbar')
+@include('admin-layout.navbar')
 @yield('content')
               <footer class="footer text-center text-sm-start">
                     &copy; <script>
@@ -36,6 +39,8 @@
                 </footer><!--end footer-->
  </div>
  </div>
+
+ 
   <script src="{{asset('/assets/js/jquery.min.js')}}"></script>
         <script src="{{asset('/assets/js/bootstrap.bundle.min.js')}}"></script>
         <script src="{{asset('/assets/js/metismenu.min.js')}}"></script>
@@ -71,64 +76,32 @@
         <script src="{{asset('/plugins/apex-charts/irregular-data-series.js')}}"></script>
         <script src="{{asset('/plugins/apex-charts/ohlc.js')}}"></script>
         <script src="{{asset('/assets/pages/jquery.apexcharts.init.js')}}"></script>
-        <!-- Session timeout js -->
-        <script src="{{asset('/plugins/bootstrap-session-timeout/bootstrap-session-timeout.min.js')}}"></script>
-        
-        <script src="{{asset('/assets/pages/jquery.animate.init.js')}}"></script>
+
         <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+       
         <!-- App js -->
         <script src="{{asset('/assets/js/app.js')}}"></script> 
-@section('script')
-   <script>     
+      
+      <script>     
+     let message = {!! json_encode(Session::get('message')) !!};
+     let msg = {!! json_encode(Session::get('alert')) !!};
+     //let logUlr = $('#frm-logout').submit();
+     //alert(msg);
+     toastr.options = {
+             timeOut: 6000,
+             progressBar: true,
+             showMethod: "slideDown",
+             hideMethod: "slideUp",
+             showDuration: 500,
+             hideDuration: 500
+         };
+     if(message != null && msg == 'success'){
+     toastr.success(message);
+     }else if(message != null && msg == 'error'){
+         toastr.error(message);
+     }
 
-
-let message = {!! json_encode(Session::get('message')) !!};
-let msg = {!! json_encode(Session::get('alert')) !!};
-let name =  {!! json_encode(auth()->user()->name) !!};
-//let logUlr = $('#frm-logout').submit();
-//alert(msg);
-toastr.options = {
-        timeOut: 6000,
-        progressBar: true,
-        showMethod: "slideDown",
-        hideMethod: "slideUp",
-        showDuration: 500,
-        hideDuration: 500
-    };
-if(message != null && msg == 'success'){
-toastr.success(message);
-}else if(message != null && msg == 'error'){
-    toastr.error(message);
-}
-$.sessionTimeout({
-message: "Hi " + name + " Your session will expire soon.",
-keepAliveUrl:"{{route('index')}}",
-logoutButton:"Logout Now",
-logoutUrl:"{{route('logouts')}}",
-redirUrl:"{{route('logouts')}}",
-warnAfter:80e3,
-redirAfter:60e4,
-countdownBar:!0
-});
-
-
-   </script>
-
-@endsection
+     </script>
         @yield('script')
-
-        <!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/61af7a68c82c976b71c03859/1fmancaoo';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
     </body>
 </html>

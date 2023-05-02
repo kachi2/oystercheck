c4ec5d5c4c87b05a91260348078eeb8df1e9f708
Michael Kachi <jesmikky@gmail.com>
2023-05-02 11:16:40 +0100

    added landing pages images

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -67,7 +67,7 @@
                                     <div class="form-group">
                                         <div class="form-label-group">
                                             <label class="form-label" for="password">Password</label>
-                                            <a class="link link-primary link-sm" tabindex="-1" href="#">Forgot Password?</a>
+                                            {{-- <a class="link link-primary link-sm" tabindex="-1" href="#">Forgot Password?</a> --}}
                                         </div>
                                         <div class="form-control-wrap">
                                             <a tabindex="-1" href="#" class="form-icon form-icon-right passcode-switch" data-target="password">

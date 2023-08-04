cee4f0f2a6b8bbea5cbf51821d81f8b13a1b33c9
Michael Ozoudeh <jesmikky@gmail.com>
2023-08-04 09:27:40 +0100

    model updated

---
+++ database/migrations/2023_08_03_121118_create_pep_saction_verifications_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePepSactionVerificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pep_saction_verifications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('verification_id')->constrained('verifications');
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('ref');
+            $table->string('first_name')->nullable();
+            $table->string('middle_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->longText('pepList')->nullable();
+            $table->longText('sanctionList')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pep_saction_verifications');
+    }
+}

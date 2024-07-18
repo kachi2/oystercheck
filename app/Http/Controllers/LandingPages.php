4210181093cf44bebe608d4ab88b154a0b80cdb1
Michael Ozoudeh <jesmikky@gmail.com>
2024-07-18 13:00:55 +0100

    update the landing pages

--- app/Http/Controllers/LandingPages.php
+++ app/Http/Controllers/LandingPages.php
@@ -10,43 +10,76 @@ class LandingPages extends Controller
 {
     //
 
+
     public function Index(){
-        return view('index');
-     }
+        return view('landing.index');
+    }
 
-     public function WhoWeAre(){
-         return view('who-we-are');
-     }
 
-     public function CoreValues(){
-         return view('core-values');
+    public function periodic(){
+        return view('landing.periodic');
+    }
+
+    public function bookdemo(){
+        return view('landing.book-a-demo');
+    }
+
+    public function transaction(){
+        return view('landing.transaction');
+    }
+
+    public function bestdecision(){
+        return view('landing.bestdecision');
+    }
+
+    public function amlsolution(){
+        return view('landing.amlsolution');
+    }
+    public function bpssClearance(){
+        return view('bpss-clearance');
+    }
+
+    public function bs7858Vetting(){
+        return view('landing.bs7858-vetting');
+    }
+
+    public function EmploymentChecks(){
+        return view('landing.employment-checks');
+    }
+
+    public function WhoWeAre(){
+         return view('landing.who-we-are');
+    }    
+
+    public function CoreValues(){
+         return view('landing.core-values');
      }
      public function AML(){
-        return view('aml');
+        return view('landing.aml');
     }
     public function ContactUs(){
-        return view('contact-us');
+        return view('landing.contact-us');
     }
     public function AboutUs(){
-        return view('about-us');
+        return view('landing.about-us');
     }
     public function Services(){
-        return view('services');
+        return view('landing.services');
     }
     public function Technology(){
-        return view('technology');
+        return view('landing.technology');
     }
     public function Mission(){
-        return view('mission');
+        return view('landing.mission');
     }
     public function Industry(){
-        return view('industry');
+        return view('landing.industry');
     }
     public function Resources(){
-        return view('resource');
+        return view('landing.resource');
     }
     public function KYC(){
-        return view('kyc');
+        return view('landing.kyc');
     }
 
     public function email(){
@@ -64,6 +97,61 @@ class LandingPages extends Controller
         mail::to('support@oysterchecks.com')->send(new AgentRequest($data));
         return redirect()->back()->with('success', 'Message send successfully');
     }
+
+    // public function Index(){
+    //     return view('index');
+    //  }
+
+    //  public function WhoWeAre(){
+    //      return view('who-we-are');
+    //  }
+
+    //  public function CoreValues(){
+    //      return view('core-values');
+    //  }
+    //  public function AML(){
+    //     return view('aml');
+    // }
+    // public function ContactUs(){
+    //     return view('contact-us');
+    // }
+    // public function AboutUs(){
+    //     return view('about-us');
+    // }
+    // public function Services(){
+    //     return view('services');
+    // }
+    // public function Technology(){
+    //     return view('technology');
+    // }
+    // public function Mission(){
+    //     return view('mission');
+    // }
+    // public function Industry(){
+    //     return view('industry');
+    // }
+    // public function Resources(){
+    //     return view('resource');
+    // }
+    // public function KYC(){
+    //     return view('kyc');
+    // }
+
+    // public function email(){
+    //     return view('emails.ClientRegistration');
+    // }
+
+    // public function ContactForm(Request $request){
+    //     $data = [
+    //         'name' => $request->name,
+    //         'email' => $request->email,
+    //         'phone' => $request->phone,
+    //         'company' => $request->company,
+    //         'address' =>$request->address,
+    //     ];
+    //     mail::to('support@oysterchecks.com')->send(new AgentRequest($data));
+    //     return redirect()->back()->with('success', 'Message send successfully');
+    // }
 
     public function knowledgeBase(){
         
--- routes/landing.php
+++ routes/landing.php
@@ -7,6 +7,22 @@ use App\Http\Controllers\LandingPages;
 
 Route::webhooks('verifications/youverify');
 #=====================LANDING PAGES ===========================
+// Route::get('/', [LandingPages::class, 'index'])->name('landing');
+// Route::get('/who-we-are', [LandingPages::class, 'WhoWeAre'])->name('who-we-are');
+// Route::get('/core-values', [LandingPages::class, 'CoreValues'])->name('core-values');
+// Route::get('/mission', [LandingPages::class, 'Mission'])->name('mission');
+// Route::get('/contact', [LandingPages::class, 'ContactUs'])->name('contact-us');
+// Route::get('/about', [LandingPages::class, 'AboutUs'])->name('about-us');
+// Route::get('/kyc', [LandingPages::class, 'KYC'])->name('kyc');
+// Route::get('/aml', [LandingPages::class, 'AML'])->name('aml');
+// Route::get('/services', [LandingPages::class, 'Services'])->name('services');
+// Route::get('/technology', [LandingPages::class, 'Technology'])->name('technology');
+// Route::get('/industry', [LandingPages::class, 'Industry'])->name('industry');
+// Route::get('/resources', [LandingPages::class, 'Resources'])->name('resource');
+// Route::post('/contact/form', [LandingPages::class, 'ContactForm'])->name('ContactForm');
+
+
+
 Route::get('/', [LandingPages::class, 'index'])->name('landing');
 Route::get('/who-we-are', [LandingPages::class, 'WhoWeAre'])->name('who-we-are');
 Route::get('/core-values', [LandingPages::class, 'CoreValues'])->name('core-values');
@@ -20,6 +36,14 @@ Route::get('/technology', [LandingPages::class, 'Technology'])->name('technology
 Route::get('/industry', [LandingPages::class, 'Industry'])->name('industry');
 Route::get('/resources', [LandingPages::class, 'Resources'])->name('resource');
 Route::post('/contact/form', [LandingPages::class, 'ContactForm'])->name('ContactForm');
+Route::get('/employment-checks', [LandingPages::class, 'EmploymentChecks'])->name('employment-checks');
+Route::get('/bpss-clearance', [LandingPages::class, 'bpssClearance'])->name('bpss-clearance');
+Route::get('/bs7858-vetting', [LandingPages::class, 'bs7858Vetting'])->name('bs7858-vetting');
+Route::get('/aml-solution', [LandingPages::class, 'amlsolution'])->name('aml-solution');
+Route::get('/best-decision', [LandingPages::class, 'bestdecision'])->name('best-decision');
+Route::get('/transaction', [LandingPages::class, 'transaction'])->name('transaction');
+Route::get('/book-a-demo', [LandingPages::class, 'bookdemo'])->name('book-a-demo');
+Route::get('/periodic', [LandingPages::class, 'periodic'])->name('periodic');
 Route::get('/frequently-asked-questions', [LandingPages::class, 'Faqs'])->name('faqs');
 Route::get('/terms-of-use', [LandingPages::class, 'Terms'])->name('terms');
 Route::get('/knowledge-base', [LandingPages::class, 'knowledgeBase'])->name('knowledgeBase');
